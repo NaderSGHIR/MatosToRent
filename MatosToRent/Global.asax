00e23bb1ad091285a52e23b3f8eead639b6bb7d1
NaderSgh <sghir.nader@hotmail.com>
2015-10-24 22:40:42 +0200

    First creation

---
+++ MatosToRent/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MatosToRent.MvcApplication" Language="C#" %>
---
+++ MatosToRent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MatosToRent.Startup))]
+namespace MatosToRent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
